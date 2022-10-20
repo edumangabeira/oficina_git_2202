b1da938e0cab0a4238bf0ca9bfe5995c1e5734e5
edu <edu.mangaba@gmail.com>
2022-10-20 12:20:28 -0300

    feat: html page

--- dashboard.r
+++
@@ -1,9 +0,0 @@
-source("extracao.r")
-source("faxina.r")
-source("transformacao.r")
-source("exibicao.r")
-
-dados <- leitura_dos_dados("dados.csv")
-dados_faxinados <- limpa_dados(dados)
-dados_sumarizados <- sumariza_dados(dados_faxinados)
-exibe_dashboard <- dados_sumarizados
